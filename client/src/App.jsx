8b548b243b4d9ef44b6883262a6cc7155bd8646d
Stephen Roth <sroth@njvc.com>
2017-12-10 11:47:26 -0600

    started rebuilding client and server builds

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+import { createStore } from 'redux';
+// import reducers from './reducers/index';
 import Home from './pages/Home';
+
+const store = createStore();
 
 const App = () => (
   <div>
@@ -9,4 +17,25 @@ const App = () => (
   </div>
 );
 
-export default App;
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Component />
+        </BrowserRouter>
+      </Provider>
+    </AppContainer>,
+    document.getElementById('reactbody'),
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    // eslint-disable-next-line
+    render(App);
+  });
+}
+
