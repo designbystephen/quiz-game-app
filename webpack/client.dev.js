ad83079a49a898f98ae738680c2207885a177ef1
Stephen Roth <designbystephen@gmail.com>
2017-12-10 10:06:53 -0600

    initial commit

---
+++ webpack/client.dev.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, '../client'),
+  devtool: 'inline-source-map',
+  resolve: { extensions: ['.js', '.jsx'] },
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/App.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, '../server/public/js'),
+    filename: './index.js',
+    publicPath: '/',
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/',
+    historyApiFallback: true
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-1'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, '../server/views/index.dev.ejs'),
+      inject: false,
+    }),
+  ],
+};
