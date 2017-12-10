d1de6dcc7a86d8dada4910db4f383637b7adf267
Stephen Roth <designbystephen@gmail.com>
2017-12-10 15:57:43 -0600

    fixed most problems being reported by linters and IDE

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,5 +10,5 @@ app.use(express.static('public'));
 app.use('/*', ssr);
 
 app.listen(3000, () => {
-  console.log('Hello World listening on port 3000!');
+  console.log('Hello World listening on port 3000!'); // eslint-disable-line
 });
--- server/routes/ssr.js
+++ server/routes/ssr.js
@@ -16,30 +16,8 @@ router.get('/', (req, res) => {
   const store = createStore(() => {});
 
   /*
-      We can dispatch actions from server side as well. This can be very useful if you want
-      to inject some initial data into the app. For example, if you have some articles that
-      you have fetched from database and you want to load immediately after the user has loaded
-      the webpage, you can do so in here.
-
-      Here we are inject an list item into our app. Normally once the user has loaded the webpage
-      we would make a request to the server and get the latest item list. But in the server we have
-      instant connection to a database (for example, if you have a mongoDB or MySQL database installed
-      in the server which contains all you items). So you can quickly fetch and inject it into the webpage.
-
-      This will help SEO as well. If you load the webpage and make a request to the server to get all the
-      latest items/articles, by the time Google Search Engine may not see all the updated items/articles.
-
-      But if you inject the latest items/articles before it reaches the user, the Search Engine will see the
-      item/article immediately.
-       */
-  // store.dispatch({
-  //   type: LIST_ACTIONS.ITEM_ADD,
-  //   item: {
-  //     name: 'middleware',
-  //     description: `Redux middleware solves different problems than Express or Koa middleware, but in a conceptually similar way.
-  //     It provides a third-party extension point between dispatching an action, and the moment it reaches the reducer.`,
-  //   },
-  // });
+    Can also dispatch here to pre-populate the store
+  */
 
   const context = {};
 
@@ -66,7 +44,7 @@ router.get('/', (req, res) => {
   } else {
     res.status(200).render('../views/index.ejs', {
       html: render(App),
-      script: JSON.stringify(finalState),
+      initialReduxState: JSON.stringify(finalState),
     });
   }
 });
