7ab68f47fd6fcdfdc0fb2c129246b68c0acd23b7
Stephen Roth <designbystephen@gmail.com>
2017-12-11 01:02:02 -0600

    added team and scoreboard styles

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -5,12 +5,14 @@ import '../styles/components/score-board.scss';
 const ScoreBoard = ({ title }) => (
   <div className="score-board">
     <div className="score-board__team">
+      <div className="score-board__score">$0</div>
       Team 1
     </div>
     <div className="score-board__title">
       { title }
     </div>
-    <div className="score-board__team">
+    <div className="score-board__team score-board__team--right">
+      <div className="score-board__score">$0</div>
       Team 2
     </div>
   </div>
